1ffaecda61c2c8328ae0c79ef134dba1a5047c5b
divanshuzinta-tmpl <divanshu.z@tunicalabsmedia.com>
2023-01-30 00:05:10 +0530

    reverted

--- controllers/getOneBlog.js
+++ controllers/getOneBlog.js
@@ -7,23 +7,14 @@ module.exports = {
     try {
       let id=req.params.id;
       if (id!=""){
-          let bg1 = await db.blogs.findOne({
+          const bg1 = await db.blogs.findOne({
             where: { id },
             include:[{
               model:db.author
             }]
         });
-        let bg2=await db.blogs.findAll({
-          where :{id : {[Sequelize.Op.ne]:bg1.id}},
-          limit : 2
-        })
-        /*
-        let blogs > findall.blogs(where blogid: [op.ne] : currentblogid, limit: 2)
-        bg1.blogs = blogs
-        */
-        let articles={bg1,bg2}
-        if(articles){
-          res.status(200).send(articles)
+        if(bg1){
+          res.send(bg1)
         }else{
           return res.status(400).send ('No blog with such id')
         }
