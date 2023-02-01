55e1d78247cc19f622308f91912c1560af56324f
divanshuzinta-tmpl <divanshu.z@tunicalabsmedia.com>
2023-02-02 00:18:49 +0530

    check1.1.1

--- controllers/feedback.js
+++ controllers/feedback.js
@@ -37,8 +37,9 @@ module.exports = {
             port: 465,
             secure: true, // true for 465, false for other ports
             auth: {
-              user: process.env.EMAIL_USER, // generated ethereal user
-              pass: process.env.EMAIL_PASS, // generated ethereal password
+              type:"login",
+              user: process.env.EMAIL_USER , // generated ethereal user
+              pass: process.env.EMAIL_PASS , // generated ethereal password
             }
           });
         
